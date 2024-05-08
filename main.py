8a5e9f4b2d60d1295f8f9869658add56fddfecb0
David Yin <29613693+E137299@users.noreply.github.com>
2024-05-08 10:32:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from import import *
+from random import randint, choice
+
+#### CLASS AND FUNCTION DEFINITIONS #####
+def playing_area():
+	t = Turtle()
+	t.speed(0)
+	t.ht()
+	t.pu()
+	t.goto(-250,250)
+	t.color("light blue")
+	t.pd()
+	t.begin_fill()
+	for i in range(4):
+		t.forward(500)
+		t.right(90)
+	t.end_fill()
+
+
+#### DRIVER CODE ####
+screen = Screen()
+screen.bgcolor("black")
+
+playing_area()
