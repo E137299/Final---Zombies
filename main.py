4cc826391d9a55d4798487e22599cb935ff5aa68
David Yin <29613693+E137299@users.noreply.github.com>
2024-05-10 13:08:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,11 +26,35 @@ Constructor( def __init__(self)):
 - will take in keys to turn left, turn right and shoot bullets.
 - player will have an attribute that is a list that stores bullets
 
+
 move(self):
 - moves object forward five pixels
+
+fire(self):
+- creates a Bullet object
+- appends the Bullet object to the players's bullet list
 '''
 class Player(Turtle):
 	pass
+
+'''
+Bullet() Class
+Constructor ( def __init__(self) ):
+- Input: player object
+- Attributes:
+	- Position: same as player
+	- Heading: same as player
+	- Player: the player
+ 
+move(self):
+- move 15 or more pixels forward
+- should call on the die() method when the bullet leaves the playing area
+
+die()
+- hides the object. 
+- removes object from the player's bullet list
+'''
+
 
 #### DRIVER CODE ####
 screen = Screen()
